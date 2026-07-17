47d3d62536422206fb62ae4ba9e42a194508269a
k0michi <k0michi@koyomi.co>
2026-07-17 13:17:43 +0900

    chore: set vite base

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
   },
-});
+  base: mode === "production" && process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`
+    : "/",
+}));
